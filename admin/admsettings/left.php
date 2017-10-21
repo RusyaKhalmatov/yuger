25b011b2b26be09744705122ffa1663eceedc1af
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-10-21 17:21:40 +0500

    translation complete

--- admin/admsettings/left.php
+++ admin/admsettings/left.php
@@ -1,18 +1,18 @@
 
-  <h3 style='text-align: center;'>Welcome to admin's page</h3>
+  <h3 style='text-align: center;'>Добро пожаловать на страницу админа</h3>
   <ul class="nav nav-pills nav-stacked">
-    <li class="active" style="text-align: center;"><a href="../../index.php">See the web site</a></li>
+    <li class="active" style="text-align: center;"><a href="../../index.php">Посмотреть сайт</a></li>
     <li class="dropdown" style="//text-align: center;">
-      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin's settings <span class="caret"></span></a>
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Настройки админа <span class="caret"></span></a>
       <ul class="dropdown-menu">
-        <li><a href="adminList.php">List of admins</a></li>
-        <li><a href="adm-profile.php">Profile settings</a></li>
-        <li><a href="add_admin.php">Add admin</a></li>                        
+        <li><a href="adminList.php">Список админов</a></li>
+        <li><a href="adm-profile.php">Настройки профиля</a></li>
+        <li><a href="add_admin.php">Добавить админа</a></li>                        
       </ul>
     </li>
-    <li><a href="#">Orders<span class="badge">5</span></a></li>
-    <li><a href="#">Mail<span class="badge">5</span></a></li>
-    <li><a href="../add_order.php">Add order</a></li>
-    <li><a href="#">Users</a></li>
-    <li><a href="../statistics.php">Statistics</a></li>
-      <!--li><a href="../check.php">Check password</a></li-->
+    <li><a href="#">Заказы<span class="badge">5</span></a></li>
+    <li><a href="#">Почта<span class="badge">5</span></a></li>
+    <li><a href="../add_order.php">Добавить продукт</a></li>
+    <li><a href="#">Пользователи</a></li>
+    <li><a href="../statistics.php">Статистика</a></li>
+    
--- admin/nav.php
+++ admin/nav.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="admin.php">Home</a>
+      <a class="navbar-brand" href="admin.php" style="margin-left: 10%;">Главная</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href=""><span class="glyphicon glyphicon-user"></span> Hi, <?= $_SESSION["name"]?></a></li>
-      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span>  Log out</a></li>
+      <li><a href=""><span class="glyphicon glyphicon-user"></span> Привет, <?= $_SESSION["name"]?></a></li>
+      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span>  Выйти</a></li>
     </ul>
   </div>
 </nav>
